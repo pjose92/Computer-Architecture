98976474c6fbdce26418de6cbca1104b83ed0544
Jose <pjose14.jp38@yahoo.com>
2020-10-08 23:03:38 -0500

    something went wrong. wrote code again

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,18 +2,21 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        
-        self.ram = [0] * 256
-        self.reg = [0] * 8
         self.pc = 0
-        self.running = False
-        
-        
+        self.reg = [0] * 10
+        self.ram = [0] * 256
+        self.cmds = {
+            0b10000010: "LDI",
+            0b01000111: "PRN",
+            0b00000001: "HLT"
+        }
+
     def load(self):
         """Load a program into memory."""
 
@@ -23,27 +26,33 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+        return self.ram[address]
 
     def trace(self):
         """
@@ -53,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -65,60 +74,31 @@ class CPU:
 
         print()
 
-    # `MAR`: Memory Address Register, holds the memory address we're reading or writing
-    # `MDR`: Memory Data Register, holds the value to write or the value just read
-
-       
-    # MAR- memory address register - address being read
-    def ram_read(self, MAR):
-        MDR = self.ram[MAR]
-        return MDR
-
-    # MAR- address being written to MDR - memory data register - data being written to address
-    def ram_write(self, MAR, MDR):
-        self.ram[MAR] = MDR
-
     def run(self):
-        """Run the CPU."""
-        # Define instruction values per specs
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
+        self.load()
 
-        # set running to True
-        self.running = True
+        running = True
 
-        # Iterate thru
-        while self.running:
-            # set instruction register
-            ir = self.ram_read(self.pc)
-            
-        # `PC`: Program Counter, address of the currently executing instruction
-        # `IR`: Instruction Register, contains a copy of the currently executing instruction
+        while running:
+            cmd = self.ram[self.pc]
+            op_size = (cmd >> 6) + 1
+
+            if self.cmds[cmd] == 'LDI':
+                reg_index = self.ram[self.pc + 1]
+                num = self.ram[self.pc + 2]
+                self.reg[reg_index] = num
+
+            elif self.cmds[cmd] == 'PRN':
+                reg_index = self.ram[self.pc + 1]
+                num = self.reg[reg_index]
+                print(num)
+
+            elif self.cmds[cmd] == 'HLT':
+                running = False
+
+            self.pc += op_size
 
 
-            # HLT/HALT: Halt CPU & exit emulator
-            if ir is HLT:
-                self.running = False
+cpu = CPU()
 
-            # LDI/LDI register immediate: Set value of register to an integer
-            elif ir is LDI:
-                # define register number
-                operand_a = self.ram_read(self.pc+1)
-                # define 8-bit immediate value
-                operand_b = self.ram_read(self.pc+2)
-                # update register
-                self.reg[operand_a] = operand_b
-                # increment pc
-                self.pc += 3
-
-            # PRN/pseudo-instruction: print numeric value stored in given register
-            elif ir is PRN:
-                # define register number
-                operand_a = self.ram_read(self.pc+1)
-                # print the value
-                print(f"Value at {operand_a}: {self.reg[operand_a]}")
-                # increment pc
-                self.pc += 2
-                
-                #returns 8
+cpu.run()
